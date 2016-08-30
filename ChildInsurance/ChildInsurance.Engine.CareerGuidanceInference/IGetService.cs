7271a1bfd63f6afa78d5dfd22f41e21b13404bd4
hemachandranhj <hemachandranhj@gmail.com>
2016-08-30 09:24:30 +0530

    Service changes

---
+++ ChildInsurance/ChildInsurance.Engine.CareerGuidanceInference/IGetService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildInsurance.Engine.CareerGuidanceInference
+{
+    [ServiceContract]
+    public interface IGetService
+    {
+        [OperationContract]
+        object GetMethod(object parameter);
+    }
+}
