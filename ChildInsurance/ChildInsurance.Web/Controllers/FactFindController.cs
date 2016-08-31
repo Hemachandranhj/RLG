f502ee0b9a07ea86aa768a147f39c2b43e8be90a
Gokul <muthu.gokul@gmail.com>
2016-08-31 15:44:54 +0530

    FactFind View

--- ChildInsurance/ChildInsurance.Web/Controllers/FactFindController.cs
+++ ChildInsurance/ChildInsurance.Web/Controllers/FactFindController.cs
@@ -3,15 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ChildInsurance.Web.Models;
+using System.IO;
+using System.Xml;
 
-namespace ChildInsurance.Web.Controllers
+namespace ChildInsurance.Web.Views
 {
     public class FactFindController : Controller
     {
         // GET: FactFind
-        public ActionResult Index()
+        public ActionResult FinancialPlanning(Guid id)
         {
-            return View();
+            FactFindViewModel model = new FactFindViewModel();
+            try
+            {
+                id = (id == null) ? Guid.NewGuid() : id;
+
+                XmlDataDocument xmldoc = new XmlDataDocument();
+                XmlNodeList xmlnode;
+                XmlNode node;
+
+                model.StudentId = id;
+                string path = HttpContext.Server.MapPath("~/App_Data/FactFind.xml");
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                xmldoc.Load(fs);
+                node = xmldoc.SelectSingleNode("StudentId");
+                xmlnode = xmldoc.GetElementsByTagName("StudentId");
+                Guid studentId = Guid.Parse(xmlnode[0].InnerText);
+                if (id == studentId)
+                {
+                    model.Asset = xmldoc.GetElementsByTagName("Asset")[0].InnerXml;
+                    model.Expenditure = xmldoc.GetElementsByTagName("Expenditure")[0].InnerXml;
+                    model.Income = xmldoc.GetElementsByTagName("Income")[0].InnerXml;
+                    model.Liablity = xmldoc.GetElementsByTagName("Liablity")[0].InnerXml;
+
+                }
+                return View(model);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SubmitPlan(FactFindViewModel model)
+        {
+
+            System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(FactFindViewModel));
+
+            string path = HttpContext.Server.MapPath("~/App_Data/FactFind.xml");
+
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, model);
+            file.Close();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ ChildInsurance/ChildInsurance.Web/Models/FactFindViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChildInsurance.Web.Models
+{
+    public class FactFindViewModel
+    {
+        public Guid StudentId { get; set; }
+        public string Asset { get; set; }
+        public string Liablity { get; set; }
+        public string Income { get; set; }
+        public string Expenditure { get; set; }
+    }
+}
