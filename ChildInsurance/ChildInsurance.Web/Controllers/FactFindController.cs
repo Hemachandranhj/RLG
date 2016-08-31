6d900de933b57032bf75e8adc3049a3a405dffd9
Gokul <muthu.gokul@gmail.com>
2016-08-31 23:31:26 +0530

    FactFind linked

--- ChildInsurance/ChildInsurance.Web/Controllers/FactFindController.cs
+++ ChildInsurance/ChildInsurance.Web/Controllers/FactFindController.cs
@@ -10,17 +10,17 @@ namespace ChildInsurance.Web.Views
     public class FactFindController : Controller
     {
         [HttpGet]
-        public ActionResult FactFind(Guid id)
+        public ActionResult Index(string type)
         {
             FactFindViewModel model = new FactFindViewModel();
 
-            id = (id == null) ? Guid.NewGuid() : id;
+            ////id = (id == null) ? Guid.NewGuid() : id;
 
             var xmldoc = new XmlDataDocument();
             XmlNodeList xmlnode;
             XmlNode node;
 
-            model.StudentId = id;
+            ////model.StudentId = id;
             string path = HttpContext.Server.MapPath("~/App_Data/FactFind.xml");
             FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
             xmldoc.Load(fs);
@@ -29,15 +29,15 @@ namespace ChildInsurance.Web.Views
             node = xmldoc.SelectSingleNode("StudentId");
             xmlnode = xmldoc.GetElementsByTagName("StudentId");
             Guid studentId = Guid.Parse(xmlnode[0].InnerText);
-            if (id == studentId)
-            {
+            ////if (id == studentId)
+            ////{
                 model.Asset = xmldoc.GetElementsByTagName("Asset")[0].InnerXml;
                 model.Expenditure = xmldoc.GetElementsByTagName("Expenditure")[0].InnerXml;
                 model.Income = xmldoc.GetElementsByTagName("Income")[0].InnerXml;
                 model.Liablity = xmldoc.GetElementsByTagName("Liablity")[0].InnerXml;
-            }
+            ////}
 
-            model.StudentId = id;
+            ////model.StudentId = id;
 
             return View(model);
         }
