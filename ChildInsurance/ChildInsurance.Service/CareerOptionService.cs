bfdd27b3eb6cb1103d394d1d1ab04f4aba83e34f
Gokul <muthu.gokul@gmail.com>
2016-08-31 13:10:43 +0530

    Service

--- ChildInsurance/ChildInsurance.Service/CareerOptionService.cs
+++ ChildInsurance/ChildInsurance.Service/CareerOptionService.cs
@@ -8,7 +8,6 @@ using ChildInsurance.Model.Service;
 
 namespace ChildInsurance.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class CareerOptionService : ICareerOptionService
     {
         public IList<string> GetCareerOptions(InterestRequest request)
