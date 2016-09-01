8cd5d568f7c4d753a68d34746d344ff065618c10
Gokul <muthu.gokul@gmail.com>
2016-09-01 10:48:04 +0530

    Illustration

---
+++ ChildInsurance/ChildInsurance.Web/Controllers/IllustrationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChildInsurance.Web.Models;
+
+namespace ChildInsurance.Web.Controllers
+{
+    public class IllustrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            string xmlData = HttpContext.Server.MapPath("~/App_Data/Illustration.xml");//Path of the xml script  
+            DataSet ds = new DataSet();//Using dataset to read xml file  
+            ds.ReadXml(xmlData);
+            IllustrationViewModel illustration = (from rows in ds.Tables[0].AsEnumerable()
+                                         select new IllustrationViewModel
+                                         {
+                                             Age = Convert.ToInt32(rows[0]),
+                                             Term = Convert.ToInt32(rows[1]),
+                                             Mode = Convert.ToString(rows[2]),
+                                             Premium = Convert.ToInt32(rows[3]),
+                                             SumAssured = Convert.ToInt32(rows[4])
+                                         }).FirstOrDefault();
+
+            return View(illustration);
+        }
+    }
+}
---
+++ ChildInsurance/ChildInsurance.Web/Models/IllustrationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChildInsurance.Web.Models
+{
+    public class IllustrationViewModel
+    {
+        public int Age { get; set; }
+        public int Term { get; set; }
+        public string Mode { get; set; }
+        public int Premium { get; set; }
+        public int SumAssured { get; set; }
+
+    }
+}
